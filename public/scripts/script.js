e526e59923b8cbfce1e7c2a057a8d6f5764da56e
chrism3lo <christopher.melopelaez@gmail.com>
2022-07-05 15:02:08 -0400

    workout page added to chris branch

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -52,4 +52,19 @@ function discount() {
 	promo.firstChild.nodeValue = "Promo Code: D25START";
 	promo.style.color = "#ff0000";
 	promo.style.fontSize = "2em";
+<<<<<<< Updated upstream
 }
+=======
+}
+
+
+
+
+document.querySelector("show-login").addEventListener("click",function(){
+	document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+	document.querySelector(".popup").classList.remove("active");
+});
+>>>>>>> Stashed changes
