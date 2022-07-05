461c5316447c14782122f01305ada18458f1f23c
chrism3lo <christopher.melopelaez@gmail.com>
2022-07-05 10:26:04 -0400

    Workout Page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -53,3 +53,11 @@ function discount() {
 	promo.style.color = "#ff0000";
 	promo.style.fontSize = "2em";
 }
+
+document.querySelector("show-login").addEventListener("click",function(){
+	document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+	document.querySelector(".popup").classList.remove("active");
+});
